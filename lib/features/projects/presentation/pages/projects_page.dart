d6e4fb48a5f3252adfa5ec63e8f02b0d05f87995
Madrigal-Carl <carlsalido.madrigal@gmail.com>
2026-06-15 23:44:14 +0800

    kpi card added in project page

--- lib/features/projects/presentation/pages/projects_page.dart
+++ lib/features/projects/presentation/pages/projects_page.dart
@@ -1,22 +1,45 @@
 import 'package:flutter/material.dart';
 
+import '../../../../app/theme/app_colors.dart';
 import '../../../../shared/widgets/header.dart';
+import '../../../../shared/widgets/kpi_card.dart';
 
 class ProjectsPage extends StatelessWidget {
   const ProjectsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: Padding(
-        padding: EdgeInsets.all(20),
+        padding: const EdgeInsets.all(20),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           spacing: 20,
           children: [
-            Header(
+            const Header(
               title: 'Projects',
               subtitle: 'Organize and track all your projects',
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: KpiCard(
+                    icon: Icons.folder_open,
+                    iconColor: AppColors.primary,
+                    name: 'Projects',
+                    number: '8',
+                  ),
+                ),
+                const SizedBox(width: 10),
+                Expanded(
+                  child: KpiCard(
+                    icon: Icons.assignment,
+                    iconColor: AppColors.priority,
+                    name: 'Tasks',
+                    number: '24',
+                  ),
+                ),
+              ],
             ),
           ],
         ),
